6e9435986d32047b529fd0f7b275668baaa7381b
Seth NeJame <sethnejame@gmail.com>
2019-10-26 09:30:44 +0200

    Adding PageNotFound, adding Switch @ Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import StatsPage from "./components/stats/StatsPage";
 import AboutPage from "./components/about/AboutPage";
+import PageNotFound from "./components/PageNotFound";
 import Header from "./components/common/Header";
 
 const App = () => (
   <>
-  <Header />
-  <div className="container-fluid">
-    <Route exact path="/" component={HomePage} />
-    <Route path="/stats" component={StatsPage} />
-    <Route path="/about" component={AboutPage} />
-  </div>
+    <Header />
+    <div className="container-fluid">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/stats" component={StatsPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
   </>
 );
 
