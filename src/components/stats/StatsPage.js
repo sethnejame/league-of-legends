fadfdb28b3ecf0a3c72201fe9389f5d9b1d11c9c
Seth NeJame <sethnejame@gmail.com>
2019-10-26 12:47:20 +0200

    Adding getPlayer api req function to stats

--- src/components/stats/StatsPage.js
+++ src/components/stats/StatsPage.js
@@ -1,33 +1,53 @@
 import React from "react";
+import axios from "axios";
 
-const StatsPage = () => (
-  <div className="jumbotron">
-    <h2>Stats</h2>
-    <p>Below you can search for stats on your favorite LoL players.</p>
-    <form>
-      <div className="form-group">
-        <label for="name">Player Name:</label>
-        <input
-          type="text"
-          className="form-control"
-          id="name"
-          placeholder="Enter Player Name..."
-        />
-      </div>
-      <div className="form-group">
-        <label for="region">Region:</label>
-        <select type="text" id="region" className="form-control">
-          <option value="" selected disabled hidden>
-            Choose Region
-          </option>
-          <option>NA1</option>
-          <option>EU</option>
-          <option>Asia</option>
-          <option>Something Else</option>
-        </select>
-      </div>
-    </form>
-  </div>
-);
+const StatsPage = () => {
+  const getPlayer = e => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://${region}.api.riotgames./com/lol/summoner/v4/summoners/by-name/${name}`,
+        {
+          headers: {
+            "X-Riot-Token": process.env.REACT_APP_PUBG_API_KEY
+          }
+        }
+      )
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div className="jumbotron">
+      <h2>Stats</h2>
+      <p>Below you can search for stats on your favorite LoL players.</p>
+      <form>
+        <div className="form-group">
+          <label for="name">Player Name:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Enter Player Name..."
+          />
+        </div>
+        <div className="form-group">
+          <label for="region">Region:</label>
+          <select type="text" id="region" className="form-control">
+            <option value="" selected disabled hidden>
+              Choose Region
+            </option>
+            <option>NA1</option>
+            <option>EU</option>
+            <option>Asia</option>
+            <option>Something Else</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default StatsPage;
