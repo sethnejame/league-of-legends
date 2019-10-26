f2ec57a56c9ad825371413be004c71a4fcdffd93
Seth NeJame <sethnejame@gmail.com>
2019-10-26 11:38:10 +0200

    Adding form input @ stats page

--- src/components/stats/StatsPage.js
+++ src/components/stats/StatsPage.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const StatsPage = () => (
   <div className="jumbotron">
     <h2>Stats</h2>
     <p>Below you can search for stats on your favorite LoL players.</p>
-    <Link to="/">Go Home</Link>
+    <form>
+      <div className="form-group">
+        <label for="name">Player Name:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          placeholder="Enter Player Name..."
+        />
+      </div>
+      <div className="form-group">
+        <label for="region">Region:</label>
+        <select type="text" id="region" className="form-control">
+          <option value="" selected disabled hidden>
+            Choose Region
+          </option>
+          <option>NA1</option>
+          <option>EU</option>
+          <option>Asia</option>
+          <option>Something Else</option>
+        </select>
+      </div>
+    </form>
   </div>
 );
 
