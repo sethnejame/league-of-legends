3b4d6be79210e6703d3076cab288d78ca4086064
Seth NeJame <sethnejame@gmail.com>
2019-10-26 13:23:14 +0200

    Fixing api func @ form

--- src/components/stats/StatsPage.js
+++ src/components/stats/StatsPage.js
@@ -34,7 +34,7 @@ const StatsPage = () => {
     <div className="jumbotron">
       <h2>Stats</h2>
       <p>Below you can search for stats on your favorite LoL players.</p>
-      <form onSubmit={e => getPlayer()}>
+      <form onSubmit={getPlayer}>
         <div className="form-group">
           <label>Player Name:</label>
           <input
