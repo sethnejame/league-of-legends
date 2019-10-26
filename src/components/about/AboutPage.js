7795f558a70e97e1fba44d0116d60d2aa73fefbf
Seth NeJame <sethnejame@gmail.com>
2019-10-26 08:22:51 +0200

    Polishing HomePage & About, moving on to Router

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+
+const AboutPage = () => (
+  <Container maxWidth="sm">
+    <Typography variant="h3" gutterBottom>
+      About
+    </Typography>
+  </Container>
+);
+
+export default AboutPage;
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,14 +3,13 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 
-const HomePage = () => {
-  return (
-    <Container maxWidth="sm">
-      <Typography variant="h3" gutterBottom>
-        League of Legends
-      </Typography>
-    </Container>
-  );
-};
+const HomePage = () => (
+  <Container maxWidth="sm">
+    <Typography variant="h3" gutterBottom>
+      League of Legends
+    </Typography>
+    <Link to="about">About</Link>
+  </Container>
+);
 
 export default HomePage;
