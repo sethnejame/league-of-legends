d5a1078c5179344f4061b95772e6926c596a444a
Seth NeJame <sethnejame@gmail.com>
2019-10-26 08:02:12 +0200

    Fixing title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Typography from "@material-ui/core/Typography";
 
 const App = () => {
   return (
-    <Typography variant="h1" component="h2" gutterBottom>
-      h1. Heading
+    <Typography variant="h3" gutterBottom>
+      League of Legends
     </Typography>
   );
 };
